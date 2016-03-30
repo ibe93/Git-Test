86df97e5bbaa2a2bafcb4a0e861b0b3a4bcdd3cf
ibe93 <ibralah@ut.utm.edu>
2016-03-30 17:13:40 -0500

    Edited main with sum function

--- main.cpp
+++ main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+void sum(int n);
+
+
 int main(){
 
+  int x;
+  
   cout << "Hello World!" << endl;
 
+  cout << "Enter a number: ";
+  cin >> x;
+
+  sum(x);
+  
+}
+
+void sum(int n){
+  int m = 1;
+  for(int i = 0; i <= n; i++){
+    m = m + i;
+    cout << "Result: " << m << endl;
+
+  }
 }
